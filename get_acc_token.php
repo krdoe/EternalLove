73ae5d9075ee5b160e99a785a19a125995893cbe
krdoe <krdoe@hotmail.com>
2015-05-22 21:51:15 +0800

    Update get_acc_token.php

--- get_acc_token.php
+++ get_acc_token.php
@@ -5,6 +5,8 @@ define("APPSECRET", "2a14f917c4ace5b21a7a217583cbf921");
 $TOKEN_URL="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$APPID."&secret=".$APPSECRET;
 $json=file_get_contents($TOKEN_URL);
 $result=json_decode($json);
+print_r($result);
 $ACC_TOKEN=$result->access_token;
+
 echo "$ACC_TOKEN"
 ?>
