e84e6a652ffb2591cdb8bca2a579e91d93988a88
krdoe <krdoe@hotmail.com>
2015-05-22 21:58:38 +0800

    Update el_create_menu.php

--- el_create_menu.php
+++ el_create_menu.php
@@ -6,7 +6,7 @@ define("APPID", "wx6fa4388ad0b35f13");
 define("APPSECRET", "2a14f917c4ace5b21a7a217583cbf921");
 
 
-$TOKEN_URL="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$APPID."&secret=".$APPSECRET;
+$TOKEN_URL="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".APPID."&secret=".APPSECRET;
 
 $json=file_get_contents($TOKEN_URL);
 $result=json_decode($json);
